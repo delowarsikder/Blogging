59133897d7017fc203fee86d92bbac7d231b9847
delowarsikder <delowarsikder099@gmail.com>
2023-05-04 10:53:45 +0600

    organize fetch data

--- frontend/src/features/posts/Post.tsx
+++ frontend/src/features/posts/Post.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Post() {
+function Post(props: any) {
+  const [title, setTitle] = useState(props.post.title);
+  const [body, setBody] = useState(props.post.body);
+  const titleElement = <h2 className='title text-start'>{title}</h2>
+  const bodyElement = <p className='card-text text-start'>{body}</p>
+
   return (
-    <div>Post</div>
+    <div>
+      <div className='row'>
+        <div className="col-8">
+          {titleElement}
+        </div>
+        <div className="col-4">
+        </div>
+      </div>
+      <div className='row'>
+        <div className="col-8">
+          {bodyElement}
+        </div>
+        <div className="col-4">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Post
+export default Post;
+
