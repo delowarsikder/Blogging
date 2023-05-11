67d4f9f6911996d1e5c1fb034ee9e709740b8e1b
delowarsikder <delowarsikder099@gmail.com>
2023-05-11 19:11:40 +0600

    create and update using single page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,12 @@ import './App.css';
 import PostEdit from './features/posts/PostEdit';
 import Posts from './features/posts/Posts';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core'
 
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, faTwitter, faFontAwesome)
 function App() {
 
   return (
--- frontend/src/features/posts/ButtonGroup.tsx
+++ frontend/src/features/posts/ButtonGroup.tsx
@@ -1,5 +1,14 @@
 import { destroyPostAsync } from "./postSlice";
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import {
+  faPenToSquare,
+  faClock, // the clock icon
+  faUserCircle, // the user circle icon
+  faCoffee, // a cup of coffee
+} from "@fortawesome/free-solid-svg-icons";
+
 
 function ButtonGroup(props: any) {
   const navigator = useNavigate();
@@ -20,10 +29,23 @@ function ButtonGroup(props: any) {
   }
 
   return (
-    <div className="btn-group">
-      <button className="btn btn-danger p-2" onClick={(e) => handleDelete(e)}>Delete</button>
-      <button className="btn btn-warning p-2" onClick={(e) => handleEdit(e)}>Edit</button>
-    </div>
+    <>
+      <i className="fa-solid fa-user">USER</i>
+      {/* <FontAwesomeIcon icon="fa-brands fa-twitter" /> */}
+      {/* <FontAwesomeIcon icon="fa-font-awesome" /> */}
+
+
+      {/* <FontAwesomeIcon icon="fa-regular fa-pen-to-square" /> */}
+      {/* <FontAwesomeIcon icon={regular("pen-to-square")} /> */}
+
+      <div className="btn-group">
+        <FontAwesomeIcon icon="check-square" />
+        <button className="btn btn-danger p-2" onClick={(e) => handleDelete(e)}>Delete</button>
+        <button className="btn btn-warning p-2" onClick={(e) => handleEdit(e)}>
+          <FontAwesomeIcon icon={faPenToSquare} />Edit</button>
+      </div>
+
+    </>
   )
 };
 
