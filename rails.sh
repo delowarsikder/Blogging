e07c79e4d1b145087c1a5f4eae1b9f639fcd9a17
delowarsikder <delowarsikder099@gmail.com>
2023-06-15 11:48:10 +0600

    configure rails server starting

---
+++ rails.sh
@@ -0,0 +1,8 @@
+#https://unix.stackexchange.com/questions/149419/how-to-check-whether-a-particular-port-is-open-on-a-machine-from-a-shell-script
+if lsof -Pi :3000 -sTCP:LISTEN -t >/dev/null ; then
+    kill -9 $(lsof -i:3001 -t)
+    rm -f tmp/pids/server.pid
+fi
+clear
+echo "rails server is starting ..."
+rails s
